6d35e41e3318f774f52495967b6d20ff4a86cadf
Palchhin <palchhinparihar@gmail.com>
2025-04-22 17:53:41 +0530

    structure: create models and routes folders, integrate routes in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 import express from "express";
+
 import connectToMongo from "./db.js";
+import authRoutes from './routes/auth.js';
+import notesRoutes from "./routes/notes.js";
 
 const app = express();
 const port = 3000;
@@ -8,8 +11,11 @@ connectToMongo();
 
 app.get('/', (req, res) => {
     res.send('Hello main page!');
-})
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/notes', notesRoutes);
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
-})
+});
