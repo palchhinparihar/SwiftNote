dadad04891a579918e4d0bb5715212550aa71cc1
Palchhin <palchhinparihar@gmail.com>
2025-04-26 10:17:45 +0530

    feat: add login route for user authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,13 +8,13 @@ const router = express.Router();
 const JWT_SECRET = "1#P5Qah9m0";
 
 // Validation rules for user input
-const checkers = [
+let checkers = [
   body('name', 'Enter a valid name').isLength({ min: 5 }),
   body('email', 'Enter a valid email').isEmail(),
   body('password', 'Enter a valid password').isLength({ min: 5 }),
 ];
 
-// Route: POST /api/auth/createuser
+// ROUTE 1: POST /api/auth/createuser
 // Desc: Create a new user
 router.post('/createuser', checkers, async (req, res) => {
   // Check if there are any errors or not
@@ -27,7 +27,7 @@ router.post('/createuser', checkers, async (req, res) => {
   try {
     // Check if a user with same email exists or not
     let user = await User.findOne({ email: req.body.email });
-    if(user) {
+    if (user) {
       return res.status(400).json({ error: "Sorry, a user with this email already exists." });
     }
 
@@ -45,14 +45,62 @@ router.post('/createuser', checkers, async (req, res) => {
     // Prepare payload for JWT
     const data = {
       user: {
-        id: user.id 
+        id: user.id
       }
     };
 
     // Generate auth token
     const authToken = jwt.sign(data, JWT_SECRET);
 
-    res.json({authToken});
+    res.json({ authToken });
+  } catch (err) {
+    // Catch the error
+    console.log(err.message);
+    res.status(500).json({ error: 'Interal Server Error', message: err.message });
+  }
+});
+
+checkers = [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password cannot be blank').exists(),
+];
+
+// ROUTE 2: POST /api/auth/login
+// Desc: Login a user
+router.post('/login', checkers, async (req, res) => {
+  // Check if there are any errors or not
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // Return a bad request
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    // Check if a user with the email exists or not
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "Please enter the correct credentials" });
+    }
+
+    // Password validation
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      return res.status(400).json({ error: "Please enter the correct credentials" });
+    }
+
+    // Prepare payload for JWT
+    const data = {
+      user: {
+        id: user.id
+      }
+    };
+
+    // Generate auth token
+    const authToken = jwt.sign(data, JWT_SECRET);
+
+    res.json({ authToken });
   } catch (err) {
     // Catch the error
     console.log(err.message);
