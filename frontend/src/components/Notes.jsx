6753e12eb1bffe421765c624aa5a84839b3aeb37
Palchhin <palchhinparihar@gmail.com>
2025-05-08 09:48:00 +0530

    refactor: extract update note modal into a separate component for better modularity.

--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import NoteContext from '../context/notes/NoteContext';
 import NoteItem from './NoteItem';
+import UpdateNote from './UpdateNote';
 
 const Notes = () => {
   const context = useContext(NoteContext);
@@ -28,37 +29,7 @@ const Notes = () => {
 
   return (
     <>
-      <button type="button" ref={ref} className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop"></button>
-      <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="staticBackdropLabel">Edit Note</h1>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-              <form className="mt-4">
-                <div className="mb-3">
-                  <label htmlFor="title" className="form-label">Title</label>
-                  <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="title" onChange={onChange} />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="description" className="form-label">Description</label>
-                  <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} />
-                </div>
-                <div className="mb-3">
-                  <label className="form-label" htmlFor="tag">Tag</label>
-                  <input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange} />
-                </div>
-              </form>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={handleSubmit}>Update Note</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <UpdateNote ref={ref} note={note} onChange={onChange} handleSubmit={handleSubmit} />
 
       <div className="row my-3">
         <h2>Your Notes</h2>
---
+++ frontend/src/components/UpdateNote.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const UpdateNote = (props) => {
+  const {ref, note, onChange, handleSubmit} = props;
+
+  return (
+    <div>
+      <button type="button" ref={ref} className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop"></button>
+      <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">Edit Note</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form className="mt-4">
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">Title</label>
+                  <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="title" onChange={onChange} />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="description" className="form-label">Description</label>
+                  <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="tag">Tag</label>
+                  <input type="text" className="form-control" id="etag" name="etag" value={note.etag ? note.etag : "General"} onChange={onChange} />
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={handleSubmit}>Update Note</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UpdateNote;
